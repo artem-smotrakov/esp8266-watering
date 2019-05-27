5f08e95826429e5444bac09aa25c2100c92bd64f
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-05-27 21:55:44 +0200

    More comments in config.py

--- config.py
+++ config.py
@@ -1,26 +1,35 @@
 import os
 import ujson
 
+# this class holds a configuration for the device
+# the configuraion is stored to a file
 class Config:
 
+    # loads a configuraion from the specified file,
+    # and initializes an instance of Config
     def __init__(self, filename):
         self.filename = filename
         self.values = {}
         self.load()
 
+    # returns a value of the specified parameter if the parameter exists
+    # otherwise, returns an empty string
     def get(self, name):
         if name in self.values:
             return self.values[name]
         return ''
 
+    # updates the specified parameter
     def set(self, name, value):
         self.values[name] = value
 
+    # stores the configuraion to the specified file
     def store(self):
         f = open(self.filename, 'w')
         f.write(ujson.dumps(self.values))
         f.close()
 
+    # loads a configration from the specified file
     def load(self):
         if not self.filename in os.listdir():
             print('cannot find ' + self.filename)
