af0aaaa67759ea7df2448df07b891307be326a24
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-05-31 20:43:41 +0200

    Upload RSA implementation

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# upload main.py to esp8266 with mpfshell
+# upload to esp8266 with mpfshell
 
 sudo mpfshell \
     -n -c \
-    "open ttyUSB0; put main.conf; put src/weather.py; put src/pump.py; \
-    put src/main.py; put src/util.py; put src/config.py; put src/http.py"
+    "open ttyUSB0; put main.conf; put key.json; \
+    lcd src; mput .*\.py; md rsa; cd rsa; lcd rsa; mput .*\.py;"
