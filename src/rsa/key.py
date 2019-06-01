f2358ac8e02f1cc77e0c6971d27770bd1f91dc09
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-06-01 07:28:00 +0200

    Removed blinding from key.py

--- src/rsa/key.py
+++ src/rsa/key.py
@@ -28,9 +28,6 @@
 
 import rsa.common
 import rsa.core
-import rsa.randnum
-import gc
-
 
 DEFAULT_EXPONENT = 65537
 
@@ -43,60 +40,6 @@ class AbstractKey(object):
     def __init__(self, n, e):
         self.n = n
         self.e = e
-
-    def blind(self, message, r):
-        """Performs blinding on the message using random number 'r'.
-        :param message: the message, as integer, to blind.
-        :type message: int
-        :param r: the random number to blind with.
-        :type r: int
-        :return: the blinded message.
-        :rtype: int
-        The blinding is such that message = unblind(decrypt(blind(encrypt(message))).
-        See https://en.wikipedia.org/wiki/Blinding_%28cryptography%29
-        """
-
-        # MicroPython 1.10 doesn't support pow() with 3 parameters
-        t = self.pow(r, self.e, self.n)
-        return (message * t) % self.n
-
-    def check_memory(self):
-        if gc.mem_free() < 2048:
-            print('collect garbage, gc.mem_free() = ' + str(gc.mem_free()) + ' bytes available')
-            gc.collect()
-            print('gc.mem_free() = ' + str(gc.mem_free()))
-
-    def pow(self, a, b, n):
-        if b == 0:
-            return 1
-
-        r = 1
-        i = 0
-        while i < b:
-            r = r * a
-
-            try:
-                r = r % n
-            except MemoryError:
-                gc.collect()
-                r = r % n
-                print('we just had some problems with memory')
-                print('but successfully recovered: ' + str(gc.mem_free()))
-
-            i = i + 1
-
-        return r
-
-    def unblind(self, blinded, r):
-        """Performs blinding on the message using random number 'r'.
-        :param blinded: the blinded message, as integer, to unblind.
-        :param r: the random number to unblind with.
-        :return: the original message.
-        The blinding is such that message = unblind(decrypt(blind(encrypt(message))).
-        See https://en.wikipedia.org/wiki/Blinding_%28cryptography%29
-        """
-
-        return (rsa.common.inverse(r, self.n) * blinded) % self.n
 
 
 class PublicKey(AbstractKey):
@@ -222,18 +165,16 @@ class PrivateKey(AbstractKey):
     def __hash__(self):
         return hash((self.n, self.e, self.d, self.p, self.q, self.exp1, self.exp2, self.coef))
 
-    def blinded_encrypt(self, message):
-        """Encrypts the message using blinding to prevent side-channel attacks.
+    def encrypt(self, message):
+        """Encrypts the message.
+        Note that it doesn't use blinding to prevent side-channel attacks
         :param message: the message to encrypt
         :type message: int
         :returns: the encrypted message
         :rtype: int
         """
 
-        blind_r = rsa.randnum.randint(self.n - 1)
-        blinded = self.blind(message, blind_r)  # blind before encrypting
-        encrypted = rsa.core.encrypt_int(blinded, self.d, self.n)
-        return self.unblind(encrypted, blind_r)
+        return rsa.core.encrypt_int(message, self.d, self.n)
 
 
 __all__ = ['PublicKey', 'PrivateKey']
