7ab236d95056eb75d7c4819e72e07c1d390e0808
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-06-03 08:15:15 +0200

    Added comments to JWT

--- src/google/auth.py
+++ src/google/auth.py
@@ -2,6 +2,9 @@ import ubinascii
 import ujson
 from rsa import pkcs1
 
+# this class builds a JWT to request an access token
+# from the Google OAuth 2.0 Authorization Server using a service account
+# see https://developers.google.com/identity/protocols/OAuth2ServiceAccount
 class JWT:
 
     def __init__(self):
@@ -15,24 +18,37 @@ class JWT:
         self.claim['exp'] = 0
         self.claim['iat'] = 0
         self.key = None
+        self.expiration = 59 * 60 # 59 minutes, in seconds
 
-    def iss(self, value):
-        self.claim['iss'] = value
+    def service_account(self, email):
+        self.claim['iss'] = email
 
+    # set a space-delimited list of the permissions
+    # that the application requests
     def scope(self, value):
         self.claim['scope'] = value
 
-    def iat(self, value):
-        self.claim['iat'] = value
-        self.claim['exp'] = value + 59 * 60 # 59 minutes, in seconds
+    # set the time the assertion was issued,
+    # specified as seconds since 00:00:00 UTC, January 1, 1970
+    def when(self, time):
+        self.claim['iat'] = time
 
+    # set an RSA private key for signing a JWT
     def private_rsa_key(self, key):
         self.key = key
 
+    # set the expiration time of the assertion,
+    # specified as seconds since 00:00:00 UTC, January 1, 1970
+    # this value has a maximum of 1 hour after the issued time
+    def expiration(self, time):
+        self.expiration = time
+
+    # build a JWT
     def build(self):
+        self.claim['exp'] = time + self.expiration
         encoded_header = ubinascii.b2a_base64(ujson.dumps(self.header))
         encoded_claim = ubinascii.b2a_base64(ujson.dumps(self.claim))
         to_be_signed = '%s.%s' % (encoded_header, encoded_claim)
-        signature =pkcs1.sign(to_be_signed, self.key, 'SHA-256')
+        signature = pkcs1.sign(to_be_signed, self.key, 'SHA-256')
         encoded_signature = ubinascii.b2a_base64(signature)
         return '%s.%s' % (to_be_signed, encoded_signature)
