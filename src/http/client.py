58c6a946aec06d3684b073fa6aefbd9ddbff31b9
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-06-04 22:07:50 +0200

    Use TLS in HttpsClient

--- src/http/client.py
+++ src/http/client.py
@@ -1,3 +1,19 @@
+try:
+    import usocket as _socket
+except:
+    import _socket
+try:
+    import ussl as ssl
+except:
+    import ssl
+
+# even if SSL/TLS is used here, MicroPython for ESP8260 doesn't support
+# validation of server certificate (at least, v1.10)
+# so, we're relatively safe if an adversary can just eavesdrop the connection
+# because all data should be encrypted
+# but if an adversary can modify the trafic, then we're in a trouble
+# because server certificate may be replaced in this case
+
 class HTTPRequest:
 
     def __init__(self):
@@ -7,6 +23,12 @@ class HTTPRequest:
         self._path = '/'
         self._headers = {}
         self._data = ''
+
+    def host(self):
+        return self._host
+
+    def port(self):
+        return self._port
 
     def method(self, method):
         self._method = method
@@ -70,13 +92,20 @@ class HTTPResponse:
     def code(self):
         return self._code
 
-class HttpClient:
+class HttpsClient:
 
-    def __init__(self):
-        self.is_tls = False
+    def __init__(sel):
+        self._request = None
 
-    def build(self):
-        pass
+    def request(self, request):
+        self._request = request
 
     def connect():
-        pass
+        if not self._request:
+            raise Exception('Holy cow! I do not have any HTTP request to send!')
+        addr = socket.getaddrinfo(self._request.host(), self._request.port())[0][-1]
+        with socket.socket() as s:
+            s.connect(addr)
+            s = ssl.wrap_socket(s)
+            s.write(self._request.build().encode())
+            return HTTPResponse.parse(s)
