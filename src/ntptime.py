a0fa977beeb7de32e904306e5a4eaf4911be1703
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-06-06 23:12:43 +0200

    Better NTP requests

--- src/ntptime.py
+++ src/ntptime.py
@@ -1,5 +1,3 @@
-# taken from https://github.com/micropython/micropython/blob/master/ports/esp8266/modules/ntptime.py
-
 try:
     import usocket as socket
 except:
@@ -9,29 +7,16 @@ try:
 except:
     import struct
 
-# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
-NTP_DELTA = 3155673600
+from socket import AF_INET, SOCK_DGRAM
 
-host = "pool.ntp.org"
+NTP_PACKET_FORMAT = '!12I'
+NTP_DELTA = 2208988800 # 1970-01-01 00:00:00
+NTP_QUERY = b'\x1b' + 47 * b'\0'
 
-def time():
-    NTP_QUERY = bytearray(48)
-    NTP_QUERY[0] = 0x1b
-    addr = socket.getaddrinfo(host, 123)[0][-1]
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(1)
-    res = s.sendto(NTP_QUERY, addr)
-    msg = s.recv(48)
-    s.close()
-    val = struct.unpack("!I", msg[40:44])[0]
-    return val - NTP_DELTA
-
-# There's currently no timezone support in MicroPython, so
-# utime.localtime() will return UTC time (as if it was .gmtime())
-def settime():
-    t = time()
-    import machine
-    import utime
-    tm = utime.localtime(t)
-    tm = tm[0:3] + (0,) + tm[3:6] + (0,)
-    machine.RTC().datetime(tm)
+def time(host="pool.ntp.org", port=123):
+    with socket.socket(AF_INET, SOCK_DGRAM) as s:
+        s.sendto(NTP_QUERY, (host, port))
+        msg, address = s.recvfrom(1024)
+        unpacked = struct.unpack(NTP_PACKET_FORMAT,
+                                 msg[0:struct.calcsize(NTP_PACKET_FORMAT)])
+        return int(unpacked[10] + unpacked[11] / 2**32 - NTP_DELTA)
