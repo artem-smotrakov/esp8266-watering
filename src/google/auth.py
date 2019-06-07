df792f111fa0337054af030f61c219eab84b643f
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-06-07 20:04:21 +0200

    Better request to gGoogle's auth server

--- src/google/auth.py
+++ src/google/auth.py
@@ -13,6 +13,13 @@ from rsa import pkcs1
 from http.core import HTTPRequest
 from http.client import HttpsClient
 
+def encode_dict_to_base64(d):
+    return encode_bytes_to_safe_base64(bytes(json.dumps(d), 'utf8'))
+
+def encode_bytes_to_safe_base64(bytes):
+    encoded = binascii.b2a_base64(bytes).replace(b'+', b'-')
+    return encoded.replace(b'/', b'_').strip().decode('utf8')
+
 # this class builds a JWT to request an access token
 # from the Google OAuth 2.0 Authorization Server using a service account
 # see https://developers.google.com/identity/protocols/OAuth2ServiceAccount
@@ -47,11 +54,11 @@ class JWTBuilder:
         time = ntptime.time()
         self._claim['iat'] = time
         self._claim['exp'] = time + self._expiration
-        encoded_header = binascii.b2a_base64(bytes(json.dumps(self._header), 'ascii')).strip().decode('ascii')
-        encoded_claim = binascii.b2a_base64(bytes(json.dumps(self._claim), 'ascii')).strip().decode('ascii')
+        encoded_header = encode_dict_to_base64(self._header)
+        encoded_claim = encode_dict_to_base64(self._claim)
         to_be_signed = '%s.%s' % (encoded_header, encoded_claim)
-        signature = pkcs1.sign(bytes(to_be_signed, 'ascii'), self._key, 'SHA-256')
-        encoded_signature = binascii.b2a_base64(signature).strip().decode('ascii')
+        signature = pkcs1.sign(bytes(to_be_signed, 'utf8'), self._key, 'SHA-256')
+        encoded_signature = encode_bytes_to_safe_base64(signature)
         return '%s.%s' % (to_be_signed, encoded_signature)
 
 class ServiceAccount:
@@ -89,6 +96,7 @@ class ServiceAccount:
         request.set_port(443)
         request.set_path('/oauth2/v4/token')
         request.set_method('POST')
+        request.set_header('Content-Type', 'application/x-www-form-urlencoded')
         request.set_data(parameters)
 
         # send the request and extract a token
